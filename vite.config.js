10aef04f74b4a0983094e5d7ca397b76d687dc89
dhurgham <dhurgham.oday@miswag.com>
2025-08-22 20:44:31 +0300

    update

--- vite.config.js
+++ vite.config.js
@@ -7,8 +7,7 @@ export default defineConfig({
         laravel({
             input: [
                 'resources/css/app.css', 
-                'resources/js/app.js',
-                'resources/css/filament/user/theme.css'
+                'resources/js/app.js'
             ],
             refresh: true,
         }),
